18ac92818f7cb527dbe2497b6e1dd207d31098e4
yoonsoyoung <hummingsy@gmail.com>
2021-12-06 23:44:29 +0900

    Comment : Add 주석 설명 추가

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -1,83 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const structjson = require('./structjson.js');
-const dialogflow = require('dialogflow');
-const uuid = require('uuid');
+const structjson = require("./structjson.js");
+const dialogflow = require("dialogflow");
+const uuid = require("uuid");
 
-const config = require('../config/keys');
+const config = require("../config/keys");
 
-const projectId = config.googleProjectID
-const sessionId = config.dialogFlowSessionID
-const languageCode = config.dialogFlowSessionLanguageCode
-
+// config/dev.js 의 내용을 불러다 넣어줌.
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
 
 // Create a new session
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-// We will make two routes 
+// We will make two routes
 
+// Text Query Route : 사용자가 보내면 그에 맞는 답
+router.post("/textQuery", async (req, res) => {
+  //We need to send some information that comes from the client to Dialogflow API
+  // 클라이언트에서 받은 정보를 dialogflow API에 보냄
+  // dialogflow npm 문서에 따라 복붙
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        // 동적으로 text를 받아오기 위함. bodyParser 란 모듈을 다운 받아 사용하고 있으므로 req.body.text 가능
+        text: req.body.text,
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
 
-// Text Query Route
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
 
-router.post('/textQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
-    // The text query request.
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            text: {
-                // The query to send to the dialogflow agent
-                text: req.body.text,
-                // The language used by the client (en-US)
-                languageCode: languageCode,
-            },
-        },
-    };
+  res.send(result);
+});
 
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
+//Event Query Route : 새로 들어오면 보이는 메세지
 
-    res.send(result)
-})
+router.post("/eventQuery", async (req, res) => {
+  //We need to send some information that comes from the client to Dialogflow API
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      event: {
+        // The query to send to the dialogflow agent
+        name: req.body.event,
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
 
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
 
-
-//Event Query Route
-
-router.post('/eventQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
-    // The text query request.
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            event: {
-                // The query to send to the dialogflow agent
-                name: req.body.event,
-                // The language used by the client (en-US)
-                languageCode: languageCode,
-            },
-        },
-    };
-
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-
-    res.send(result)
-})
-
-
-
-
-
-
+  res.send(result);
+});
 
 module.exports = router;
